029fa269d816a373c1c574297e0cc22ca93a47c4
ssuyash3000 <ssuyash2001@gmail.com>
2023-10-26 01:15:28 +0530

    adds reducer for redux store

---
+++ src/redux/reducers/todoReducers.js
@@ -0,0 +1,40 @@
+import { TOGGLE_TODO } from "../actions/todoActions";
+
+const initialState = {
+  todos: [],
+};
+
+export function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todo: [
+          ...state.todos,
+          {
+            text: action.text,
+            time: new Date().getTime(),
+            status: "Not Done",
+            completionTime: null,
+          },
+        ],
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (time === todo.time) {
+            return {
+              ...todo,
+              status: todo.status === "Not Done" ? "Done" : "Not Done",
+              completionTime:
+                todo.status === "Not Done" ? new Date().getTime() : null,
+            };
+          }
+          return todo;
+        }),
+      };
+    default:
+      return state;
+  }
+}
