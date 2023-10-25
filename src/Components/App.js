14a827acb63d54cbebafdb8756ac54a868399439
ssuyash3000 <ssuyash2001@gmail.com>
2023-10-25 13:27:01 +0530

    optimises toggleTodo function

--- src/Components/App.js
+++ src/Components/App.js
@@ -18,19 +18,19 @@ function App() {
     setTodos((prevState) => [...prevState, newTodo]);
   };
   const toggleTodo = (status, time) => {
-    if (status === "Not Done") {
-      let currTodo = todos.filter((todo) => todo.time === time);
-      let newTodoList = todos.filter((todo) => todo.time !== time);
-      currTodo[0].status = "Done";
-      currTodo[0].completionTime = new Date().getTime();
-      setTodos([...newTodoList, currTodo[0]]);
-    } else {
-      let currTodo = todos.filter((todo) => todo.time === time);
-      let newTodoList = todos.filter((todo) => todo.time !== time);
-      currTodo[0].status = "Not Done";
-      currTodo[0].completionTime = null;
-      setTodos([...newTodoList, currTodo[0]]);
-    }
+    setTodos((prevTodos) => {
+      const todoIndex = prevTodos.findIndex((todo) => {
+        return time === todo.time;
+      });
+      if (status === "Not Done") {
+        prevTodos[todoIndex].status = "Done";
+        prevTodos[todoIndex].completionTime = new Date().getTime();
+      } else {
+        prevTodos[todoIndex].status = "Not Done";
+        prevTodos[todoIndex].completionTime = null;
+      }
+      return [...prevTodos];
+    });
   };
   const removeTodo = (time) => {
     let newTodoList = todos.filter((todo) => todo.time !== time);
